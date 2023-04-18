535cbbfd0a7c078afc475cfa24632f0efb23dc6b
nnassar0716 <99611556+nnassar0716@users.noreply.github.com>
2023-04-18 17:45:57 -0400

    edited GET routes

--- flask-app/src/personal/personal.py
+++ flask-app/src/personal/personal.py
@@ -4,6 +4,7 @@ from src import db
 
 personal = Blueprint('personal', __name__)
 
+
 # Simple GET route that gets given user's personal transactions
 @personal.route('/personal/<userID>', methods=['GET'])
 def get_personal_transactions(userID):
@@ -20,5 +21,32 @@ def get_personal_transactions(userID):
 
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
+    
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
 
-    return jsonify(json_data)
+    return the_response
+
+# Simple GET route that lists all personal transactions of 
+@personal.route('/personal/<userID>/<catID>', methods=['GET'])
+def get_personal_transactions(userID, catID):
+
+    cursor = db.get_db().cursor()
+
+    cursor.execute('SELECT * FROM PersonalTransactions WHERE user_id = {0}'.format(userID) + 'category_id = {0}'.format(catID))
+
+    column_headers = [x[0] for x in cursor.description]
+
+    json_data = []
+
+    theData = cursor.fetchall()
+
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+    
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+
+    return the_response

36ec753a205e3973e75b43937c6c827f5ddbb360
Zoe Garbus <zoegarbus@Zoes-MacBook-Pro.local>
2023-04-18 18:04:44 -0400

    w users POST route

--- flask-app/src/users/users.py
+++ flask-app/src/users/users.py
@@ -22,3 +22,28 @@ def get_users():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+# adding a POST route to register a new user
+@users.route('/registerUser', methods=['POST'])
+def register_user():
+    current_app.logger.info('Processing form data')
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
+
+    user_fname = req_data['fname']
+    user_lname = req_data['lname']
+    user_street = req_data['street_line']
+    user_city = req_data['city']
+    user_state = req_data['state']
+    user_country = req_data['country']
+
+    insert_stmt = 'INSERT INTO Users (fName, lName, address, city, state, country) VALUES ("'
+    insert_stmt += user_fname + '", "' + user_lname + '", ' + user_street + '", ' + user_city + '", ' + user_state + '", ' + user_country + ')'
+
+    current_app.logger.info(insert_stmt)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(insert_stmt)
+    db.get_db().commit()
+    return "Success"
+

9e1fc8aff84dd2cfeb3c0dc784606f1a9e95aef0
nnassar0716 <99611556+nnassar0716@users.noreply.github.com>
2023-04-18 21:06:06 -0400

    added routes for mediums

--- flask-app/src/personal/personal.py
+++ flask-app/src/personal/personal.py
@@ -62,6 +62,7 @@ def get_cards(userID):
     
     return jsonify(json_data)
 
+# Registers a new card for a user 
 @personal.route('registerCard/<userID>', methods=['POST'])
 def register_card(userID):
     req_data = request.get_json()
@@ -70,7 +71,8 @@ def register_card(userID):
     sec_code = req_data['secCode']
     zip_num = req_data['zip']
 
-    insert_stmt = 'INSERT INTO Cards (cardNum, secCode, zip) VALUES (" ' + card_num + '", "' + sec_code + '", "' + zip_num + ')'
+    insert_stmt = 'INSERT INTO Cards (user_id, cardNum, secCode, zip) VALUES (" '
+    insert_stmt += '{0}'.format(userID) + '", "' + card_num + '", "' + sec_code + '", "' + zip_num + '")'
 
     cursor = db.get_db().cursor()
     cursor.execute(insert_stmt)
@@ -82,15 +84,51 @@ def register_card(userID):
 @personal.route('/addPersonal/<userID>', methods=['POST'])
 def add_personal_transaction(userID):
     req_data = request.get_json()
-    
+
     transaction_amnt = req_data['amount']
     transaction_desc = req_data['description']
     transaction_cat = req_data['category']
     transaction_debcred = req_data['deb_cred']
     transaction_med = req_data['medium']
 
-    insert_stmt = 'INSERT INTO PersonalTransactions (amount, description, category_id, debOrCred, medium_id) VALUES (" '
-    insert_stmt += transaction_amnt + '", "' + transaction_desc + '", "' + transaction_cat + '", "' + transaction_debcred + '", "' + transaction_med + ')'
+    insert_stmt = 'INSERT INTO PersonalTransactions (user_id, amount, description, category_id, debOrCred, medium_id) VALUES (" '
+    insert_stmt += '{0}'.format(userID) + '", "' + transaction_amnt + '", "' + transaction_desc + '", "' + transaction_cat + '", "' + transaction_debcred + '", "' + transaction_med + ')'
+
+    cursor = db.get_db().cursor()
+    cursor.execute(insert_stmt)
+    db.get_db().commit()
+    return "Success"
+
+# Gets all the mediums a user has registered
+@personal.route('/getMediums/<userID>', methods=['GET'])
+def get_mediums(userID):
+
+    cursor = db.get_db().cursor()
+
+    cursor.execute('SELECT * FROM Mediums WHERE user_id = {0}'.format(userID))
+
+    column_headers = [x[0] for x in cursor.description]
+
+    json_data = []
+
+    theData = cursor.fetchall()
+
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+    
+    return jsonify(json_data)
+
+
+# Registers a new medium for a user 
+@personal.route('registerMedium/<userID>', methods=['POST'])
+def register_medium(userID):
+    req_data = request.get_json()
+
+    medium_name = req_data['name']
+    card_num = req_data['cardNum']
+
+    insert_stmt = 'INSERT INTO Mediums (user_id, name, cardNum) VALUES (" '
+    insert_stmt += '{0}'.format(userID) + '", "' + medium_name + '", "' + card_num + '")'
 
     cursor = db.get_db().cursor()
     cursor.execute(insert_stmt)
